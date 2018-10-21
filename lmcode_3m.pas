de36ff74e6dd0e8767d375c6cd7d5ddb948aad1a
Dima <dserp18@gmail.com>
2018-10-21 09:57:29 +0300

    Update lmcode_3m.pas

--- lmcode_3m.pas
+++ lmcode_3m.pas
@@ -127,3 +127,23 @@ writeln('acc = ',acc);
 writeln('z_flag = ',z_flag);
 writeln('pz_flag = ',pz_flag);
 end.
+
+
+{--------
+ INP
+ STA 20
+ INP 
+ sta 21
+ inp 
+ sta 22
+ lda 23
+ add 22
+ sta 23
+ lda 20
+ sub 21
+ sta 20
+ brp 06
+ lda 23
+ OUT
+ HLT
+ --------}
