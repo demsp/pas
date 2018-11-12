a64ed1c09d135605228e2e24c52f6cc482384cd7
demsp <dserp18@gmail.com>
2018-11-12 13:34:24 +0300

    Update pol1.pas

--- arif_expess/polish_two/pol1.pas
+++ arif_expess/polish_two/pol1.pas
@@ -29,9 +29,6 @@ begin
  j:=1;   // нумерация элементов массива начинается с единицы
  i:=1;
  size := 0; {Изначально стек пуст}
- //readln(str_arr);       //считываем строку
-
-//str_arr:='[[[+]]]'; 
 str_arr:='[+ 4 3]'; 
  
  prev:
@@ -51,11 +48,6 @@ str_arr:='[+ 4 3]';
 end;
     if (str_arr[i]='+') then sight:='+';
     if (str_arr[i]='-') then sight:='-';
-   // if (str_arr[i]='-') then data_arr[j]:= data_arr[j]-1;
-   // if (str_arr[i]='>') then j:=j+1;
-   // if (str_arr[i]='<') then j:=j-1;
-   // if (str_arr[i]='.') then write(chr(data_arr[j]));
-   
    // скобки
     if (str_arr[i]='[') then begin 
     if(bracket_flag=0) then Push(i);
@@ -67,18 +59,11 @@ end;
       str_arr[i]:=' ';
       bracket_flag:=1;
       Pop();
-      //if (data_arr[j]>0) then 
-      // begin
         i := a[size+1];
         goto prev;
-      // end;
       end;
  i:=i+1;
  goto prev;
  next:
-for k:=1 to 10 do begin 
-write(data_arr[k]);
-write(' ');
-end;
 writeln(str_arr);
 end.
