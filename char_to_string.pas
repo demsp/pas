ba16a9aa56133dee942440a7e31ee62036c6026f
demsp <dserp18@gmail.com>
2019-05-02 11:32:41 +0300

    Update char_to_string.pas

--- char_to_string.pas
+++ char_to_string.pas
@@ -1,1 +1,9 @@
-var c:char; s:string; begin s:='abcdef'; c:=s[2]; s[3]:=c; writeln(c);writeln(s);end.
+var c:char; 
+s:string; 
+begin 
+s:='abcdef'; 
+c:=s[2]; 
+s[3]:=c; 
+writeln(c);
+writeln(s);
+end.
