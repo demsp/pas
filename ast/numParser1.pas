29c8474a90e81fcf4124236a4e8f9b5df730c96b
demsp <dserp18@gmail.com>
2019-05-03 06:59:25 +0300

    Create numParser1.pas

---
+++ ast/numParser1.pas
@@ -0,0 +1,23 @@
+ var
+ str_arr: string;                     
+ i: integer;
+ digit_flag:integer;
+     acc: integer; 
+begin
+ digit_flag:=0; 
+ str_arr:='123';
+ for i:=1 to length(str_arr) do begin    // в цикле обрабатываем строку
+   if(ord(str_arr[i])>=49) and (ord(str_arr[i])<=57)  then begin
+      if (digit_flag=0) then begin 
+                          digit_flag:=1;
+                           writeln('acc0=',acc);
+                          end;
+  if (digit_flag=1) then begin
+                          acc:=acc*10;
+                          acc:=acc+(integer(str_arr[i])-integer('0'));
+                          writeln('acc1=',acc);
+                         end;
+  end;
+ end;
+ writeln(acc);
+end.
