fed983440eda1d9566a30419fedce7854fc70ce9
demsp <dserp18@gmail.com>
2022-11-01 01:06:01 +0300

    Update workpiece-billet.pas

--- abcd/workpiece-billet.pas
+++ abcd/workpiece-billet.pas
@@ -1,12 +1,13 @@
 var
-    srt:string[100];
-    var_acc:integer;
-    i: integer;
-    arr:array[0..3];
+  str:string[30];
+  var_acc:integer;
+  i: integer;
+  arr:array[0..3] of integer; 
   begin
     str:=' a=2 b=3 ';
-    while i<length(str) do 
-     begin
-     //... 
+   while i<length(str) do 
+   begin
+     if Ord(str[i])>=97 then if Ord(str[i])<=100 then print(str[i]);
+     i:=i+1;
      end;    
   end.
