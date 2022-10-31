082dab62e4eaac83f30c7dedbb1526a564dc856c
demsp <dserp18@gmail.com>
2022-10-31 23:24:09 +0300

    Create workpiece-billet.pas

---
+++ abcd/workpiece-billet.pas
@@ -0,0 +1,12 @@
+var
+    srt:string[100];
+    var_acc:integer;
+    i: integer;
+    arr:array[0..3];
+  begin
+    str:=' a=2 b=3 ';
+    while i<length(str) do 
+     begin
+     //... 
+     end;    
+  end.
