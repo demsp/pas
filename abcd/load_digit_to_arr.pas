e663a844a43d0cb5a824a965f4d2abba8ebd2877
demsp <dserp18@gmail.com>
2022-11-05 17:27:10 +0300

    Update load_digit_to_arr.pas

--- abcd/load_digit_to_arr.pas
+++ abcd/load_digit_to_arr.pas
@@ -3,13 +3,15 @@ var
   var_acc:integer;
   i,j: integer;
   arr:array[0..3] of integer; 
+  
   begin
     str:=' a=2 b=3 ';
+    
    while i<length(str) do 
    begin
      if Ord(str[i])>=97 then if Ord(str[i])<=100 then var_acc:=Ord(str[i]);
      if Ord(str[i])>=48 then if Ord(str[i])<=58 then arr[var_acc-97]:=Ord(str[i])-48;       
      i:=i+1;
      end; 
-     for j:=0 to 3 do writeln(arr[j]);
+     for j:=0 to 3 do write(arr[j],' ');
   end.
